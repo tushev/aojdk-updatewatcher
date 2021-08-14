4a315ea1e339bb6433ef22dc49abe10851d104c3
Simon Tushev <tushev@users.noreply.github.com>
2021-08-14 23:48:33 +0500

    bump version

--- src/Properties/Settings.Designer.cs
+++ src/Properties/Settings.Designer.cs
@@ -73,7 +73,7 @@ namespace AJ_UpdateWatcher.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("2.0.2.0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("2.0.3.0")]
         public string SettingsFormatVersion {
             get {
                 return ((string)(this["SettingsFormatVersion"]));
